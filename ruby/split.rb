f8bb477aa3329299e65fdd34ded49eb53f824101
Jonas Minnberg <sasq64@gmail.com>
2021-09-11 08:06:12 +0200

    Windows fix

--- ruby/split.rb
+++ ruby/split.rb
@@ -1,11 +1,10 @@
 #console.enabled = false
 # Sprite split and tween example
-img = Image.from_file("data/ball.png")
-sprites = img.split(3, 4).map { |img| add_sprite(img) }
-scale = 10.0
+img = Image.from_file("data/face.png")
+sprites = img.split(16, 4).map { |img| add_sprite(img) }
+scale = 4.0
 sprites.each do | s |
     s.pos = s.pos * scale
-    p "#{s.pos.x} #{s.pos.y}"
     tween(s).seconds(5.0).fn(:out_back).
       from(pos: s.pos + Vec2.rand(5000, 5000)).
       from(rotation: 4 * rand(Math::PI * 2)).
